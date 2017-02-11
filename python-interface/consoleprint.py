1e31d88615ee3585d03e687b9f38834d29bf4c05
Danny Yang <dzy4@cornell.edu>
2017-02-11 15:09:15 -0500

    python for loading/printing commands

---
+++ python-interface/consoleprint.py
@@ -0,0 +1,14 @@
+def move_forward(dist):
+  print("Moving forward "+str(dist))
+
+def move_backward(dist):
+  print("Moving backward " +str(dist))
+
+def turn_left(angle):
+  print("Turning left " + str(angle))
+
+def turn_right(angle):
+  print("Turning right "+ str(angle))
+
+def wait(time):
+  print("Waiting "+ str(time))
