0061cd3bd115bdb1995af486b0ccb12c2265fc8a
Alan Yan <alan20854@gmail.com>
2017-02-15 21:54:28 -0500

    configured the pi to receive script, still working on part of it

--- python-interface/tcp.py
+++ python-interface/tcp.py
@@ -1,14 +1,34 @@
 from socket import *
 serverPort = 3001
 serverSocket= socket(AF_INET, SOCK_STREAM)
-serverSocket.bind( ('', serverPort) )
+ip = "0.0.0.0"
+serverSocket.bind( (ip, serverPort) )
 serverSocket.listen(1)
 print('Running')
+connectionSocket, addr = serverSocket.accept()
+command = ""
 while True:
-	connectionSocket, addr = serverSocket.accept()
-	message = connectionSocket.recv(1024).decode()
-	received=open("received.py",'w')
-	received.write(message)
-	received.close()
-	import received
-	connectionSocket.close()
+	command += connectionSocket.recv(1024).decode()
+        if command.find(">>>") > 0:
+                break
+	#print command
+received=open("received.py",'w')
+received.write(command)
+received.close()
+import received
+connectionSocket.close()
+
+"""
+while True:
+        while True:
+                command += connectionSocket.recv(1024).decode()
+                if command.find(">>>>") > 0:
+                        print command
+                        received=open("received.py",'w')
+                        received.write(command)
+                        received.close()
+                        import received
+                        break
+connectionSocket.close()
+
+"""
