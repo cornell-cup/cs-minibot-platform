beae00f0544024f01f869f6684e24fb4dbb5e9a0
Celine <cc972@cornell.edu>
2017-02-26 14:09:56 -0500

    download fcn update

--- src/main/resources/public/js/blockly.js
+++ src/main/resources/public/js/blockly.js
@@ -66,26 +66,17 @@ function destroyClickedElement(event) { document.body.removeChild(event.target);
 
 /* DOWNLOAD ATTEMPT V3 */
 
-function download() {
-  downloadScript(getb)
-}
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
 
-function downloadScript(data, filename, type) {
-    var a = document.createElement("a"),
-        file = new Blob([data], {type: type});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);  
-        }, 0); 
-    }
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
 
 
