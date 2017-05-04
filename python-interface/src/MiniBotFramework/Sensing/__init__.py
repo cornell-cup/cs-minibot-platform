c1cf859ed782752eb3a02b38bf1c98eb960d6b64
Celine <cc972@cornell.edu>
2017-05-04 13:19:24 -0400

    import fix

--- python-interface/src/MiniBotFramework/Sensing/__init__.py
+++ python-interface/src/MiniBotFramework/Sensing/__init__.py
@@ -1,4 +1,5 @@
 import MiniBotFramework.Sensing.Sensor
+import MiniBotFramework.Sensing.ColorSensor
 import MiniBotFramework.Sensing.GPIOSensor
 from MiniBotFramework.Lib.TCS34725 import TCS34725 as CSensor
 import smbus
