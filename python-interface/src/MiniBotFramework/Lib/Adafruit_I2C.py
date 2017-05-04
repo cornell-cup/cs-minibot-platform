9575e63304c1092b6b5a7a232fa6e51d3f07c240
Celine <cc972@cornell.edu>
2017-05-04 13:55:56 -0400

    FIXES

--- python-interface/src/MiniBotFramework/Lib/Adafruit_I2C.py
+++ python-interface/src/MiniBotFramework/Lib/Adafruit_I2C.py
@@ -94,7 +94,7 @@ class Device(object):
         if i2c_interface is None:
             # Use pure python I2C interface if none is specified.
             # import Adafruit_PureIO.smbus
-            self._bus = smbus.SMBus(busnum)
+            self._bus = SMBus(busnum)
         else:
             # Otherwise use the provided class to create an smbus interface.
             self._bus = i2c_interface(busnum)
