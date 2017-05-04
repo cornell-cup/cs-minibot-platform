f666557ec47201af59be3198e90801861ce04c0f
Celine <cc972@cornell.edu>
2017-05-04 13:29:59 -0400

    Added I2C to lib

--- python-interface/src/MiniBotFramework/Lib/__init__.py
+++ python-interface/src/MiniBotFramework/Lib/__init__.py
@@ -1,1 +1,4 @@
 import MiniBotFramework.Controls.Lib.legopi
+
+# import smbus (Saves installing extra stuff for TRPG tutorials... If needed uncomment this and install smbus software + turn on i2c)
+import smbus
--- python-interface/src/MiniBotFramework/Sensing/__init__.py
+++ python-interface/src/MiniBotFramework/Sensing/__init__.py
@@ -2,3 +2,4 @@ import MiniBotFramework.Sensing.Sensor
 import MiniBotFramework.Sensing.ColorSensor
 import MiniBotFramework.Sensing.GPIOSensor
 import smbus
+import MiniBotFramework.Lib.Adafruit_I2C
