7911f60acfe2a58c8815501f9e8c2bb3ddda4fbe
Celine <cc972@cornell.edu>
2017-05-05 22:44:11 -0400

    haphzarad deletions

--- python-interface/src/MiniBotFramework/Sensing/ColorSensor.py
+++ python-interface/src/MiniBotFramework/Sensing/ColorSensor.py
@@ -1,7 +1,16 @@
 from MiniBotFramework.Sensing.Sensor import Sensor
 from MiniBotFramework.Lib.TCS34725 import TCS34725 as CSensor
-import MinibotFramework.Lib.minibot_tools
 import smbus, math, time
+
+def distance(p1, p2):
+    """ Returns distance between two 3-tuples. 
+    Used for evaluating color """
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+
+def normalize(vector):
+    """ Returns a 3-element vector as a unit vector. """
+    sum = vector[0] + vector[1] + vector[2]
+    return (vector[0]/(sum+0.0), vector[1]/(sum+0.0), vector[2]/(sum+0.0))
 
 class ColorSensor(Sensor):
     """ Pre-determined set of colors and corresponding RGB 3-tuples
