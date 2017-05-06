739998e5d55d5282f30f3cb33b38f197198c9806
Celine <cc972@cornell.edu>
2017-05-05 22:42:30 -0400

    haphazard import fixes

--- python-interface/src/MiniBotFramework/Lib/__init__.py
+++ python-interface/src/MiniBotFramework/Lib/__init__.py
@@ -2,3 +2,4 @@ import MiniBotFramework.Lib.legopi
 import MiniBotFramework.Lib.Adafruit_I2C
 # import smbus (Saves installing extra stuff for TRPG tutorials... If needed uncomment this and install smbus software + turn on i2c)
 import smbus
+import MiniBotFramework.Lib.minibot_tools
