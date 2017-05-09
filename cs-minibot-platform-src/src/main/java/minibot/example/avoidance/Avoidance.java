2f1840f4119161759cfc66d9e9555b2e7704da3c
Jimmy Chen <jzc8@cornell.edu>
2017-05-09 09:43:28 -0400

    Experiment with arrrays in matlabcontrol

--- cs-minibot-platform-src/src/main/java/minibot/example/avoidance/Avoidance.java
+++ cs-minibot-platform-src/src/main/java/minibot/example/avoidance/Avoidance.java
@@ -6,7 +6,10 @@ import matlabcontrol.MatlabConnectionException;
 import matlabcontrol.MatlabInvocationException;
 import matlabcontrol.MatlabProxy;
 import matlabcontrol.MatlabProxyFactory;
-import simulator.Simulator;
+import matlabcontrol.trunk.matlabcontrol.src.matlabcontrol.extensions.MatlabNumericArray;
+import matlabcontrol.trunk.matlabcontrol.src.matlabcontrol.extensions.MatlabTypeConverter;
+
+import java.util.Arrays;
 
 
 public class Avoidance extends Thread {
@@ -34,6 +37,27 @@ public class Avoidance extends Thread {
         MatlabProxy proxy = factory.getProxy();
         //Display 'hello world' just like when using the demo
         proxy.eval("disp('hello world')");
+        double[] x_vals = new double[] {1,2,3,4,5,6};
+        double [][] x = new double[1][1];
+        x[0] = x_vals;
+        double[] y_vals = new double[] {2,4,6,8,10,12}; //matlabcontrol.trunk.matlabcontrol.src.matlabcontrol.extensions
+        MatlabTypeConverter processor = new MatlabTypeConverter(proxy);
+        double[][] blank = new double[1][1];
+        processor.setNumericArray("x_vals", new MatlabNumericArray(x,
+                        blank));
+        proxy.eval("array = transpose(x_vals)");
+        double[][] transposedArray = processor.getNumericArray("x_vals")
+                .getRealArray2D();
+
+        //Print the returned array, now transposed
+        System.out.println("Transposed: ");
+        for(int i = 0; i < transposedArray.length; i++)
+        {
+            System.out.println(Arrays.toString(transposedArray[i]));
+        }
+
+//Disconnect the proxy from MATLAB
+
 
 //Disconnect the proxy from MATLAB
         proxy.disconnect();
