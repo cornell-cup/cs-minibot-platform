5042b03bdc7bcce9bb588140efd56da0767d92ab
Trevor Edwards <tre23@cornell.edu>
2017-05-17 14:21:07 -0400

    add matlab scripts folder

---
+++ matlab-scripts/main.m
@@ -0,0 +1,1 @@
+[u1, u2, tracker] = Controller_mustafa(x_p,y_p,theta,x_vals,y_vals,tracker);
---
+++ matlab-scripts/main_m.m
@@ -0,0 +1,2 @@
+[u1, u2, tracker] = Controller_mustafa(x_p,y_p,theta,x_vals,y_vals,tracker);
+u1=real(u1);
